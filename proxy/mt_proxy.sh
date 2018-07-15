5a896aa768c1bab4ab07fcca92c0cd85e3be8f3c
shellhub <shellhub.me@gmail.com>
2018-07-15 23:13:54 +0800

    basic complete

--- proxy/mt_proxy.sh
+++ proxy/mt_proxy.sh
@@ -29,12 +29,54 @@ init_release(){
   OS=`echo "$OS" | tr '[:upper:]' '[:lower:]'`
 
   if [[ $OS = *'ubuntu'* || $OS = *'debian'* ]]; then
-    pkg='apt'
+    PM='apt'
   elif [[ $OS = *'centos'* ]]; then
-    pkg='yum'
+    PM='yum'
   else
     exit 1
   fi
 }
 
-init_release
+install_dependency()
+{
+  init_release
+  if [[ $PM = 'apt' ]]; then
+    apt install git curl build-essential libssl-dev zlib1g-dev -y
+  elif [[ $PM = 'yum' ]]; then
+    #statements
+    yum install openssl-devel zlib-devel -y
+    yum groupinstall "Development Tools" -y
+  fi
+}
+
+compile_source()
+{
+  git --version 2>&1 >/dev/null # improvement by tripleee
+  GIT_IS_AVAILABLE=$?
+  if [[ $GIT_IS_AVAILABLE -eq 0 ]]; then
+    if [[ ! -d "MTProxy" ]]; then
+      git clone https://github.com/TelegramMessenger/MTProxy
+    fi
+    cd MTProxy
+    make && cd objs/bin
+  else
+    $PM install git -y
+    compile_source
+  fi
+}
+
+main()
+{
+  install_dependency
+  compile_source
+  curl -s https://core.telegram.org/getProxySecret -o proxy-secret
+  curl -s https://core.telegram.org/getProxyConfig -o proxy-multi.conf
+  SECRET=$(head -c 16 /dev/urandom | xxd -ps)
+
+  read -p "Input server port:" SERVER_PORT
+  echo $SERVER_PORT
+  # read -p "Input secret (defalut $SERVER_PORT)：" SECRET
+  # echo $SECRET
+  ./mtproto-proxy -u nobody -p 8888 -H ${SERVER_PORT} -S ${SECRET} --aes-pwd proxy-secret proxy-multi.conf -M 1
+}
+main
