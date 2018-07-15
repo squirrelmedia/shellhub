d48e74b82af3701e4958dbc989974c82880fbc8c
shellhub <shellhub.me@gmail.com>
2018-07-15 21:59:55 +0800

    rename file

---
+++ utils/method.sh
@@ -0,0 +1,40 @@
+init_release(){
+  if [ -f /etc/os-release ]; then
+      # freedesktop.org and systemd
+      . /etc/os-release
+      OS=$NAME
+  elif type lsb_release >/dev/null 2>&1; then
+      # linuxbase.org
+      OS=$(lsb_release -si)
+  elif [ -f /etc/lsb-release ]; then
+      # For some versions of Debian/Ubuntu without lsb_release command
+      . /etc/lsb-release
+      OS=$DISTRIB_ID
+  elif [ -f /etc/debian_version ]; then
+      # Older Debian/Ubuntu/etc.
+      OS=Debian
+  elif [ -f /etc/SuSe-release ]; then
+      # Older SuSE/etc.
+      ...
+  elif [ -f /etc/redhat-release ]; then
+      # Older Red Hat, CentOS, etc.
+      ...
+  else
+      # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
+      OS=$(uname -s)
+  fi
+
+  # convert string to lower case
+  echo $OS
+  OS='centos linux'
+  OS=`echo "$OS" | tr '[:upper:]' '[:lower:]'`
+  echo $OS
+  if [[ $OS = *'ubuntu'* || $OS = *'debian'* ]]; then
+    pkg='apt'
+  elif [[ $OS = *'centos'* ]]; then
+    pkg='yum'
+  else
+    exit 1
+  fi
+  echo $pkg
+}
