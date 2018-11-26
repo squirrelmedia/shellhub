1c1324c1625929e088b77aaba020260df134b05b
shellhub <shellhub.me@gmail.com>
2018-11-26 17:07:11 +0800

    support install choice

--- ssmgr/ssmgr.sh
+++ ssmgr/ssmgr.sh
@@ -83,32 +83,35 @@ config(){
   templ=`cat ./ss.template.yml`
   printf "$config\ncat << EOF\n$templ\nEOF" | bash > ss.yml
 
-  # write ip address
-  echo "IP=$(dig +short myip.opendns.com @resolver1.opendns.com)" >> config
+  read -p "Install webgui(website) y/n?" webgui
+  if [[ $webgui = "y" ]]; then
+    # write ip address
+    echo "IP=$(dig +short myip.opendns.com @resolver1.opendns.com)" >> config
+    # write email username
+    read -p "Input your email address:" email_username
+    echo "email_username=${email_username}" >> config
 
-  # write email username
-  read -p "Input your email address:" email_username
-  echo "email_username=${email_username}" >> config
+    # write email password
+    read -p "Input your email password:" email_password
+    echo "email_password=${email_password}" >> config
 
-  # write email password
-  read -p "Input your email password:" email_password
-  echo "email_password=${email_password}" >> config
+    # write alipay config
+    read -p "Input alipay appid:" alipay_appid
+    echo "alipay_appid=${alipay_appid}" >> config
 
-  # write alipay config
-  read -p "Input alipay appid:" alipay_appid
-  echo "alipay_appid=${alipay_appid}" >> config
+    read -p "Input alipay_private_key:" alipay_private_key
+    echo "alipay_private_key=${alipay_private_key}" >> config
 
-  read -p "Input alipay_private_key:" alipay_private_key
-  echo "alipay_private_key=${alipay_private_key}" >> config
+    read -p "Input alipay_public_key:" alipay_public_key
+    echo "alipay_public_key=${alipay_public_key}" >> config
 
-  read -p "Input alipay_public_key:" alipay_public_key
-  echo "alipay_public_key=${alipay_public_key}" >> config
-
-  # generate webgui.yml
-  config=`cat ./config`
-  templ=`cat ./webgui.template.yml`
-  printf "$config\ncat << EOF\n$templ\nEOF" | bash > webgui.yml
-
+    # generate webgui.yml
+    config=`cat ./config`
+    templ=`cat ./webgui.template.yml`
+    printf "$config\ncat << EOF\n$templ\nEOF" | bash > webgui.yml
+  else
+    echo "no webgui selected!!!"
+  fi
 }
 
 install_ssmgr(){
